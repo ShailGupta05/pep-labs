68c9966f6e30217f7b1b362eed352fba2fdde856
ShailGupta05 <shailgupta34@gmail.com>
2023-01-16 21:33:46 +0000

    Saved progress on Average 2023-01-16 21:33:46 +0000

--- Average/src/main/java/Average.java
+++ Average/src/main/java/Average.java
@@ -19,10 +19,12 @@ public class Average {
      */
     public double getAvg(int[] nums){
         double avg=0;
+       ;
         for(int i=0;i<nums.length;i++){
-            avg=(avg + nums[i])/(nums.length ) ;
-
+            avg=(avg + nums[i])/nums.length ;
+         
         }
         return avg;
+
     }
 }

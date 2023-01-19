1a5f3f8d33529640c0c8e042b381dc25c0eab662
ShailGupta05 <shailgupta34@gmail.com>
2023-01-19 21:37:29 +0000

    Saved progress on CreateTable 2023-01-19 21:37:29 +0000

--- CreateTable/problem1.sql
+++ CreateTable/problem1.sql
@@ -1,6 +1,6 @@
  CREATE TABLE song(
-        titel var(255),
-        artist var(255)  
+        title varchar(255),
+        artist varchar(255)  
         );
         insert into song (title, artist) value (Let it be,Beatles);
          insert into song (title, artist) value (Imagine,Beatles);

dd6a26d8420c20c09b103dd78809ee60e340c7b5
ShailGupta05 <shailgupta34@gmail.com>
2023-01-19 21:53:38 +0000

    Saved progress on StaticKeyword 2023-01-19 21:53:38 +0000

--- StaticKeyword/src/main/java/Runner.java
+++ StaticKeyword/src/main/java/Runner.java
@@ -22,7 +22,7 @@ public class Runner {
      * @param in the String value you should change global_var to in.
      */
     public void changeClassVar(String in){
-    static  String global_var= in;
+    Class.global_var= in;
 
     }
 }

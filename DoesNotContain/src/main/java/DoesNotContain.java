e20a685acea0e938ec496c29ecdfd98b93c1e860
ShailGupta05 <shailgupta34@gmail.com>
2023-01-31 21:27:54 +0000

    Saved progress on DoesNotContain 2023-01-31 21:27:54 +0000

--- DoesNotContain/src/main/java/DoesNotContain.java
+++ DoesNotContain/src/main/java/DoesNotContain.java
@@ -16,9 +16,9 @@ public class DoesNotContain {
         
         for(int i=0;i<arr.length;i++){
 
-            if( arr[i]!=target)
+            if( arr[i]==target)
             {
-                return true;
+                return false;
     
             }
              
@@ -26,7 +26,7 @@ public class DoesNotContain {
     
             }
          
-         return false;
+         return true;
     }}
     
 
